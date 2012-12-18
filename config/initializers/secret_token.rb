413e5ebf8612f62d7f8158fc73ad803d94cab49e
Juho Salmio <salmiju@Juhos-MacBook-Pro.local>
2012-12-18 20:41:48 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sauna::Application.config.secret_token = '25b097af5a4d8bd994c43024ab3078beccdf7dd9293cba76e83b90a9651df73b976b5c3d2719bf15a318f06a50cda96a0e82950625f274b78b2d5969b08e4c34'
